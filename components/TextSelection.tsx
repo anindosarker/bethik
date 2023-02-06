0b8050d7338f88f1f2ef1873c39b3d5f8ea2643b
anindo <sarkeranindo@gmail.com>
2023-02-06 17:21:18 +0600

    range works with some bugs2

--- components/TextSelection.tsx
+++ components/TextSelection.tsx
@@ -2,36 +2,59 @@ import React, { useState } from "react";
 
 const TextSelection = () => {
   const [text, setText] = useState("Three fruits are: apples, mango, banana.");
-  const [startIndex, setStartIndex] = useState(0);
-  const [endIndex, setEndIndex] = useState(0);
+  const [index, setIndex] = useState({ start: 0, end: 0 });
+
+  const startIndexHandler = (e) => {
+    //check if the start index is less than the end index
+    if (e.target.value < index.end) {
+      console.log("start index.. ", e.target.value);
+      console.log("end index.. ", index.end);
+
+      setIndex({
+        ...index,
+        start: e.target.value
+      });
+    }
+  };
+
+  //another function for the end index
+  const endIndexHandler = (e) => {
+    //check if the end index is greater than the start index
+    console.log("start index.. ", index.start);
+    console.log("end index.. ", e.target.value);
+
+    if (e.target.value > index.start) {
+      setIndex({
+        ...index,
+        end: e.target.value
+      });
+    }
+  };
 
   return (
-    <div>
-      <div className="flex flex-col">
-        {text}
-        <div className="flex justify-between space-x-4">
-          <input
-            type="range"
-            min={0}
-            max={text.length}
-            step={1}
-            value={startIndex}
-            onChange={(e) => setStartIndex(e.target.value)}
-          />{" "}
-          <br />
-          <input
-            type="range"
-            min={0}
-            max={text.length}
-            step={1}
-            value={endIndex}
-            onChange={(e) => setEndIndex(e.target.value)}
-          />
-        </div>
+    <div className="flex flex-col">
+      <div className="text-lg">
+        {text.slice(0, index.start)}
+        <span className="bg-red-500">{text.slice(index.start, index.end)}</span>
+        {text.slice(index.end)}
       </div>
-
-      <div className="flex">
-        Selected Text: {text.slice(startIndex, endIndex)}
+      <div className="flex justify-between space-x-4">
+        <input
+          type="range"
+          min={0}
+          max={text.length}
+          step={1}
+          value={index.start}
+          onChange={startIndexHandler}
+        />
+        <input
+          type="range"
+          min={0}
+          max={text.length}
+          step={1}
+          value={index.end}
+          onChange={endIndexHandler}
+        />
       </div>
     </div>
   );
