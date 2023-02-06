c264ad8a02961b74043a1907773564fc72424dab
anindo <sarkeranindo@gmail.com>
2023-02-06 17:32:25 +0600

    slider working

--- components/TextSelection.tsx
+++ components/TextSelection.tsx
@@ -1,50 +1,56 @@
 import React, { useState } from "react";
 
+const dataPrompt = "আমার দেশের নাম, তুমি কি জানো?";
+
 const TextSelection = () => {
-  const [text, setText] = useState("Three fruits are: apples, mango, banana.");
+  const [text, setText] = useState(dataPrompt);
   const [index, setIndex] = useState({ start: 0, end: 0 });
 
   const startIndexHandler = (e) => {
     //check if the start index is less than the end index
     if (e.target.value < index.end) {
-      console.log("start index.. ", e.target.value);
-      console.log("end index.. ", index.end);
-
       setIndex({
         ...index,
-        start: e.target.value
+        start: e.target.value,
       });
     }
   };
 
   //another function for the end index
   const endIndexHandler = (e) => {
-    //check if the end index is greater than the start index
-    console.log("start index.. ", index.start);
-    console.log("end index.. ", e.target.value);
-
-    if (e.target.value > index.start) {
+    if (e.target.value < index.start) {
+      setIndex({
+        end: index.start,
+        start: e.target.value,
+      });
+    } else {
       setIndex({
         ...index,
-        end: e.target.value
+        end: e.target.value,
       });
     }
   };
 
+
+  
   return (
     <div className="flex flex-col">
       <div className="text-lg">
         {text.slice(0, index.start)}
-        <span className="bg-red-500">{text.slice(index.start, index.end)}</span>
+        <span className="bg-red-500 text-white">
+          {text.slice(index.start, index.end)}
+        </span>
         {text.slice(index.end)}
       </div>
-      <div className="flex justify-between space-x-4">
+      <div className="flex justify-between space-x-4 mt-4">
+        Start
         <input
           type="range"
           min={0}
           max={text.length}
           step={1}
           value={index.start}
+          className=""
           onChange={startIndexHandler}
         />
         <input
@@ -55,6 +61,7 @@ const TextSelection = () => {
           value={index.end}
           onChange={endIndexHandler}
         />
+        End
       </div>
     </div>
   );
