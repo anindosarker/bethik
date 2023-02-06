5ca867e75b92e3ed334279e1f8511db466e6a2a0
anindo <sarkeranindo@gmail.com>
2023-02-07 01:12:12 +0600

    slider fixed

--- components/TextSelection.tsx
+++ components/TextSelection.tsx
@@ -1,51 +1,17 @@
 import React, { useState } from "react";
-import MultiRangeSlider from "./multiRangeSlider/MultiRangeSlider";
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
 
-const text = "আমার দেশের নাম, তুমি কি জানো?";
+const text =
+  "আমার দেশের নাম, তুমি কি জানো?আমার দেশের নাম, তুমি কি জানো?আমার দেশের নাম, তুমি কি জানো?আমার দেশের নাম, তুমি কি জানো?আমার দেশের নাম, তুমি কি জানো?";
 
 const TextSelection = () => {
   const [selectedText, setSelectedText] = useState(text);
   const [index, setIndex] = useState({ start: 0, end: 0 });
 
-  const startIndexHandler = (e) => {
-    console.log("start index...", index.start);
-    console.log("end index...", index.end);
-    console.log("event ...", e.target.value);
-
-    //check if the start index is less than the end index
-    if (e.target.value < index.end) {
-      setIndex({
-        ...index,
-        start: e.target.value,
-      });
-    }
-    setSelectedText(text.slice(e.target.value, index.end));
-  };
-
-  //another function for the end index
-  const endIndexHandler = (e) => {
-    console.log("start index...", index.start);
-    console.log("end index...", index.end);
-    console.log("event ...", e.target.value);
-
-    if (e.target.value < index.start) {
-      setIndex({
-        end: index.start,
-        start: e.target.value,
-      });
-    } else {
-      setIndex({
-        ...index,
-        end: e.target.value,
-      });
-    }
-
-    setSelectedText(text.slice(index.start, e.target.value));
-  };
-
   return (
-    <div className="flex flex-col">
-      <div className="text-lg">
+    <div className="flex flex-col max-w-screen-md">
+      <div className="text-lg max-w-sm">
         {text.slice(0, index.start)}
         <span className="bg-red-500 text-white">
           {text.slice(index.start, index.end)}
@@ -53,17 +19,22 @@ const TextSelection = () => {
         {text.slice(index.end)}
       </div>
       <div className="flex justify-between space-x-4 mt-4">
-        <MultiRangeSlider
+        <RangeSlider
+          id="range-slider"
+          step={1}
           min={0}
           max={text.length}
-          onChange={({ min, max }) => {
-            setIndex({ start: min, end: max });
+          onInput={(e) => {
+            setIndex({
+              start: e[0],
+              end: e[1],
+            });
           }}
         />
       </div>
-      <div className="mt-4 flex space-x-4">
+      <div className="mt-4 flex space-x-4 max-w-sm">
         <div className="">Incorrect: </div>
-        <div className="border border-black whitespace-pre	">
+        <div className="border border-black whitespace-pre-wrap">
           {text.slice(index.start, index.end)}
         </div>
       </div>
