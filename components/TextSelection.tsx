b7a99a777f280b5bcd68116afeeaf8349c47a63f
anindo <sarkeranindo@gmail.com>
2023-02-06 15:54:33 +0600

    selected with highlight

--- components/TextSelection.tsx
+++ components/TextSelection.tsx
@@ -1,24 +1,40 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 
 const TextSelection = () => {
+  const [text, setText] = useState("Sample text here");
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(0);
   const [selectedText, setSelectedText] = useState("");
-  const textRef = useRef(null);
 
-  const handleSelection = () => {
-    if (textRef.current) {
-      const selected = window.getSelection().toString();
-      setSelectedText(selected);
-    }
+  const handleSelection = (start, end) => {
+    setStartIndex(start);
+    setEndIndex(end);
+    setSelectedText(text.slice(start, end));
   };
 
   return (
-    <div>
-      <div ref={textRef} onMouseUp={handleSelection}>
-        আমার দেশের নাম, তুমি কি জানো?
+    <div className="flex flex-col items-center">
+      <div className="text-xl">
+        {text.split("").map((char, index) => (
+          <span
+            key={index}
+            className={
+              index >= startIndex && index < endIndex
+                ? "bg-gray-200"
+                : "bg-transparent"
+            }
+            onMouseDown={() => handleSelection(index, index + 1)}
+            onMouseOver={(event) => {
+              if (event.buttons === 1) {
+                handleSelection(startIndex, index + 1);
+              }
+            }}
+          >
+            {char}
+          </span>
+        ))}
       </div>
-      <br />
-      <br />
-      Selected Text: {selectedText}
+      <div className="text-lg mt-4">Selected text: {selectedText}</div>
     </div>
   );
 };
