403d68c82c3ecf4423e074b9f824cb668b2b2847
anindo <sarkeranindo@gmail.com>
2023-02-07 12:19:56 +0600

    button added

---
+++ components/Dualdivs.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type Props = {
+  selectedText?: string;
+};
+
+function Dualdivs({ selectedText }: Props) {
+  return (
+    <div className="flex flex-col w-full">
+      <section>
+        {/* Incorrect */}
+        <div className="">Incorrect: </div>
+        <div className="border-2 border-red-500 whitespace-pre-wrap rounded-xl">
+          {selectedText}
+        </div>
+      </section>
+
+      <section>
+        {/* Correct */}
+        <div className="">Correct: </div>
+        <textarea className="w-full border-2 border-green-500 whitespace-pre-wrap rounded-xl">
+          {selectedText}
+        </textarea>
+      </section>
+    </div>
+  );
+}
+
+export default Dualdivs;
--- components/TextSelection.tsx
+++ components/TextSelection.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
-
+import Dualdivs from "./Dualdivs";
 
 const text =
   "আমার দেশের নাম, তুমি কি জানো?আমার দেশের নাম, তুমি কি জানো?আমার দেশের নাম, তুমি কি জানো?আমার দেশের নাম, তুমি কি জানো?আমার দেশের নাম, তুমি কি জানো?";
@@ -10,33 +10,43 @@ const TextSelection = () => {
   const [index, setIndex] = useState({ start: 0, end: 0 });
 
   return (
-    <div className="flex flex-col max-w-screen-md">
-      <div className="text-lg max-w-sm">
+    <div className="flex flex-col">
+      <section className="text-lg max-w-sm">
+        {/* Text Display with highlighted selection */}
+
         {text.slice(0, index.start)}
         <span className="bg-red-500 text-white">
           {text.slice(index.start, index.end)}
         </span>
         {text.slice(index.end)}
-      </div>
-      <div className="flex justify-between space-x-4 mt-4">
-        <RangeSlider 
-          id="range-slider" 
-          step={1}
-          min={0}
-          max={text.length}
-          onInput={(e: any) => {
-            setIndex({
-              start: e[0],
-              end: e[1],
-            });
-          }}
-        />
-      </div>
-      <div className="mt-4 flex space-x-4 max-w-sm">
-        <div className="">Incorrect: </div>
-        <div className="border border-black whitespace-pre-wrap">
-          {text.slice(index.start, index.end)}
+
+        {/* Range Slider */}
+
+        <div className="mt-4 mb-4">
+          <RangeSlider
+            id="range-slider"
+            step={1}
+            min={0}
+            max={text.length}
+            onInput={(e: any) => {
+              setIndex({
+                start: e[0],
+                end: e[1],
+              });
+            }}
+          />
         </div>
+      </section>
+
+      {/* DualDivs: Text Display with highlighted selection */}
+      <section className="max-w-sm">
+        <Dualdivs selectedText={text.slice(index.start, index.end)} />
+      </section>
+
+      <div className="flex justify-center">
+        <button className="bg-blue-500 rounded-3xl text-white w-1/2 p-2">
+          Add another +
+        </button>
       </div>
     </div>
   );
