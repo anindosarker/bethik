2ebaaa43646fff8b62c0339baa66b833c22ea9eb
anindo <sarkeranindo@gmail.com>
2023-02-09 01:56:53 +0600

    works before reset

--- components/Dualdivs.tsx
+++ components/Dualdivs.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { React, useRef } from "react";
 
 type Props = {
-  selectedText?: string;
+  selectedText?: string[];
+  correctionHandler: Function;
+  resetHandler: Function;
 };
 
-function Dualdivs({ selectedText }: Props) {
+function Dualdivs({ selectedText, correctionHandler, resetHandler }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <div className="flex flex-col w-full">
       <section>
@@ -15,7 +18,34 @@ function Dualdivs({ selectedText }: Props) {
         </div>
       </section>
 
-     
+      <section>
+        {/* Incorrect */}
+        <div className="">Correct: </div>
+        <input
+          ref={inputRef}
+          className="border-2 border-blue-500 whitespace-pre-wrap rounded-xl px-2"
+          defaultValue={selectedText?.join(" ")}
+        />
+      </section>
+
+      <button
+        className="bg-green-500 rounded-3xl text-white h-20 w-40 p-4 m-4"
+        onClick={() => {
+          correctionHandler(inputRef.current?.value, selectedText?.join(" "));
+          inputRef.current!.value = "";
+        }}
+      >
+        Correct Kor
+      </button>
+      <button
+        className="bg-red-500 rounded-3xl text-white h-20 w-40 p-4 m-4"
+        onClick={() => {
+         resetHandler();
+         inputRef.current!.value = "";
+        }}
+      >
+        Reset Selection
+      </button>
     </div>
   );
 }
--- components/TextSelection.tsx
+++ components/TextSelection.tsx
@@ -1,20 +1,92 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Dualdivs from "./Dualdivs";
+import { start } from "repl";
 
 const text =
-  "আমার দেশের নাম, তুমি কি জানো? ২০১৪ সালের ৫ই জানুয়ারির আগ পর্যন্ত তাই ছিল অঘোষিত নিয়ম। মুসলমানরা তিনটি প্রদেশে সংখ্যাগরিষ্ঠ হলেও সামগ্রিকভাবে ভারতে তারা সংখ্যালঘু।";
-
+  "আমার দেশের নাম, তুমি কি জানো?২০১৪ সালের ৫ই জানুয়ারির আগ পর্যন্ত তাই ছিল অঘোষিত নিয়ম।";
 
 function TextSelection() {
+  const [selectedWords, setSelectedWords] = useState<string[]>([]);
+  const [selectedRange, setSelectedRange] = useState<
+    | {
+        start: number;
+        end: number;
+      }
+    | undefined
+  >(undefined);
+
+  const [storedCorrections, setStoredCorrections] = useState<
+    {
+      incorrectText: string;
+      correctedText: string;
+      start: number;
+      end: number;
+    }[]
+  >([]);
+
+  const handleClick = (index: number) => {
+    let start = selectedRange?.start;
+    let end = selectedRange?.end;
+    if (start === undefined || end === undefined) {
+      start = index;
+      end = index;
+    } else {
+      end = index;
+      if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
+      }
+    }
+    setSelectedRange({
+      start: start,
+      end: end,
+    });
+    setSelectedWords(text.split(" ").slice(start, end + 1));
+  };
+
+  const correctionHandler = (submittedText: any, incText: any) => {
+    if (selectedRange) {
+      setStoredCorrections((prevSelectedRange) => {
+        return [
+          ...prevSelectedRange,
+          {
+            incorrectText: incText,
+            correctedText: submittedText,
+            start: selectedRange?.start,
+            end: selectedRange?.end,
+          },
+        ];
+      });
+      resetHandler();
+    }
+
+    console.log("storedCorrection", storedCorrections);
+  };
+
+  const resetHandler = () => {
+    setSelectedRange(undefined);
+    setSelectedWords([]);
+  };
+
   return (
     <div>
-      {text.split(" ").map((word, index) => (
-        <span key={index}>{word} </span>
-      ))}
+      {/* Display Original Text */}
+      <div>
+        {text.split(" ").map((word, index) => (
+          <span key={index} onClick={() => handleClick(index)}>
+            {word}{" "}
+          </span>
+        ))}
+      </div>
 
       {/* Display clicked words */}
       <div>
-        
+        <Dualdivs
+          selectedText={selectedWords}
+          correctionHandler={correctionHandler}
+          resetHandler={resetHandler}
+        />
       </div>
     </div>
   );
