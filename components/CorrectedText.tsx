b29b583be391920c80fc3f98fa2a83dae9198fb4
anindo <sarkeranindo@gmail.com>
2023-02-09 13:42:12 +0600

    design

--- components/CorrectedText.tsx
+++ components/CorrectedText.tsx
@@ -19,13 +19,32 @@ function CorrectedText({ words, originalText }: Props) {
   const text = newTextHandler(words);
 
   return (
-    <div>
-      <h2 className="text-3xl p-4">New Text: </h2>
+    <div className="flex-col space-y-4 p-4 bg-slate-100 rounded-xl mt-4"> 
+      <h2 className="text-3xl">New Text</h2>
+       <div className="text-sm text-gray-500">
+            Replaced words are highlighted in yellow
+          </div>
       <div className="text-xl border border-green-600 p-4 rounded-full bg-green-200">
-        {text}
+        {text.split(" ").map((word, index) => (
+          <span
+            key={index}
+            className={`inline-block m-2 ${highlightText(words, word)}`}
+          >
+            {word}{" "}
+          </span>
+        ))}
       </div>
     </div>
   );
+}
+
+function highlightText(words: any, word: string) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].correctedText === word) {
+      return "bg-yellow-200";
+    }
+  }
+  return "";
 }
 
 export default CorrectedText;
--- components/Dualdivs.tsx
+++ components/Dualdivs.tsx
@@ -14,27 +14,38 @@ function Dualdivs({ selectedText, correctionHandler, resetHandler }: Props) {
   }, [selectedText]);
 
   return (
-    <div className="flex flex-col w-full">
-      <section>
+    <div className="flex flex-col w-full space-y-4">
+      <section className="flex justify-between items-center space-x-4">
         {/* Incorrect */}
         <div className="">Incorrect: </div>
 
-        {selectedText.length ? (
-          <div
-            className="border-2 border-red-500 whitespace-pre-wrap rounded-xl px-2"
-            placeholder="Select words by clicking"
-          >
-            {selectedText}
-          </div>
-        ) : (
-          <div className="text-sm text-gray-500">
-            "Select words by clicking"
-          </div>
-        )}
+        <div className="text-sm">
+          {selectedText.length ? (
+            <div
+              className="border-2 border-red-500 whitespace-pre-wrap rounded-xl px-2"
+            >
+              {selectedText.join(" ")}
+            </div>
+          ) : (
+            <div className="text-sm text-gray-500">
+              "Select words by clicking"
+            </div>
+          )}
+        </div>
+
+        <button
+          className="bg-red-500 rounded-full text-white p-2 text-sm"
+          onClick={() => {
+            resetHandler();
+            setInputValue("");
+          }}
+        >
+          Reset Selection
+        </button>
       </section>
 
-      <section>
-        {/* Incorrect */}
+      <section className="flex justify-between items-center space-x-4">
+        {/* Correct */}
         <div className="">Correct: </div>
         <input
           className="border-2 border-blue-500 whitespace-pre-wrap rounded-xl px-2"
@@ -43,26 +54,16 @@ function Dualdivs({ selectedText, correctionHandler, resetHandler }: Props) {
             setInputValue(e.target.value);
           }}
         />
+        <button
+          className="bg-green-500 rounded-full text-white p-2 text-sm"
+          onClick={() => {
+            correctionHandler(inputValue, selectedText?.join(" "));
+            setInputValue("");
+          }}
+        >
+          Correct Kor
+        </button>
       </section>
-
-      <button
-        className="bg-green-500 rounded-3xl text-white h-20 w-40 p-4 m-4"
-        onClick={() => {
-          correctionHandler(inputValue, selectedText?.join(" "));
-          setInputValue("");
-        }}
-      >
-        Correct Kor
-      </button>
-      <button
-        className="bg-red-500 rounded-3xl text-white h-20 w-40 p-4 m-4"
-        onClick={() => {
-          resetHandler();
-          setInputValue("");
-        }}
-      >
-        Reset Selection
-      </button>
     </div>
   );
 }
