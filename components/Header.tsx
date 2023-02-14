c77b320f79a7f97fc6b21473adc73fed874569ae
anindo <sarkeranindo@gmail.com>
2023-02-14 07:57:34 +0600

    refactor nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,16 +13,17 @@ export default function Header() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header aria-label="Site Header" className="shadow-sm">
-        <div className="mx-auto max-w-screen-xl p-4">
-          <div className="flex items-center justify-between gap-4 lg:gap-10">
-            <div className="flex lg:w-0 lg:flex-1">
-              <a href="#">
-                <span className="sr-only">Logo</span>
-                <span className="h-10 w-20 rounded-lg bg-gray-200"></span>
-              </a>
-            </div>
-            {session && (
+      {session && (
+        <header aria-label="Site Header" className="shadow-sm">
+          <div className="mx-auto max-w-screen-xl p-4">
+            <div className="flex items-center justify-between gap-4 lg:gap-10">
+              <div className="flex lg:w-0 lg:flex-1">
+                <a href="#">
+                  <span className="sr-only">Logo</span>
+                  <span className="h-10 w-20 rounded-lg bg-gray-200"></span>
+                </a>
+              </div>
+
               <nav
                 aria-label="Site Nav"
                 className="hidden gap-8 text-sm font-medium md:flex"
@@ -34,25 +35,7 @@ export default function Header() {
                   Dashboard
                 </Link>
               </nav>
-            )}
 
-            {!session ? (
-              <div className="hidden flex-1 items-center justify-end gap-4 sm:flex">
-                <a
-                  className="rounded-lg bg-gray-100 px-5 py-2 text-sm font-medium text-gray-500"
-                  href=""
-                >
-                  Log in
-                </a>
-
-                <a
-                  className="rounded-lg bg-blue-600 px-5 py-2 text-sm font-medium text-white"
-                  href=""
-                >
-                  Sign up
-                </a>
-              </div>
-            ) : (
               <div className="hidden flex-1 items-center justify-end gap-4 sm:flex">
                 <button
                   className="flex items-center bg-red-50 rounded-full text-red-500 py-2 text-sm px-4"
@@ -63,34 +46,34 @@ export default function Header() {
                   Log out{" "}
                 </button>
               </div>
-            )}
 
-            <div className="lg:hidden">
-              <button
-                className="rounded-lg bg-gray-100 p-2 text-gray-600"
-                type="button"
-              >
-                <span className="sr-only">Open menu</span>
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+              <div className="lg:hidden">
+                <button
+                  className="rounded-lg bg-gray-100 p-2 text-gray-600"
+                  type="button"
                 >
-                  <path
-                    d="M4 6h16M4 12h16M4 18h16"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                  />
-                </svg>
-              </button>
+                  <span className="sr-only">Open menu</span>
+                  <svg
+                    aria-hidden="true"
+                    className="h-5 w-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4 6h16M4 12h16M4 18h16"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </header>
+        </header>
+      )}
     </>
   );
 }
